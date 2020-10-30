c21e21dd4af89d41a359b02338639fcb654aaffe
Timothy Casey <tvcasey@yahoo.com>
2020-10-29 22:47:54 -0400

    Organized project by reviewing possible methods to solve the steps that will create a flashcard game.  I'm starting to understand things, but there is still a lot to learn.  Mike helped me to figure out my npm start issues to much effect.  Also my local hosts are running.  React is a great way to code and I like its plasticity.  It's ordered well too.  Have a long way to go, but it is possible to produced a decent finished product.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React from 'react';
+import axios from 'axios';
+
+
+
+
+//Goals:
+//Use componentDidMount to make axios call to API (localhost:5000)
+//Save response from API as state variable data
+//Try to display the 'word' from the first card in the first collection
+
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      data: [],
+      currentCollection: 0,
+      currentCard: 0,
+
+
+
+
+      componentDidMount() {
+        axios.get('http://localhost:5000/api/collections')
+          .then(res => {
+            const cards = res.data;
+            this.setState({ cards });
+          })
+      }
+
+      //data: comes from the axios call
+      //Other state variables you may want: currentCollection(int), currentCard(int), frontOfCard(bool)
+    }
+  }
+  
+  
+  render(){
+    return (
+      <div>
+        <ul>
+        { this.state.cards(cards => <li>{cards}</li>)}
+      </ul> 
+      </div>
+    );
+    }
 }
 
 export default App;
