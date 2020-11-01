f5be40912d39e857bcd32a7561170d2554cd0770
Timothy Casey <tvcasey@yahoo.com>
2020-10-31 22:29:31 -0400

    Already tried to push up was not able get the push to work.  It says it's one commit behind.  Was able through the .push to populate the 3 collections of cards with words and definitions.  Used for loops and indexes to organize the information.  Tony gave some good direction and I finally seem to be getting this, if only to a small degree right now.

--- src/App.js
+++ src/App.js
@@ -4,47 +4,72 @@ import React from 'react';
 import axios from 'axios';
 
 
-
-
-//Goals:
-//Use componentDidMount to make axios call to API (localhost:5000)
-//Save response from API as state variable data
-//Try to display the 'word' from the first card in the first collection
-
 class App extends React.Component {
-  constructor(props){
-    super(props)
-    this.state={
-      data: [],
-      currentCollection: 0,
-      currentCard: 0,
-
-
-
-
-      componentDidMount() {
-        axios.get('http://localhost:5000/api/collections')
-          .then(res => {
-            const cards = res.data;
-            this.setState({ cards });
-          })
-      }
-
-      //data: comes from the axios call
-      //Other state variables you may want: currentCollection(int), currentCard(int), frontOfCard(bool)
+    constructor(props) {
+        super(props)
+        this.state = {
+            cards: [],
+            currentCollection: 0,
+            currentCard: 0,
+            loading: true
+        }
     }
-  }
-  
-  
-  render(){
-    return (
-      <div>
-        <ul>
-        { this.state.cards(cards => <li>{cards}</li>)}
-      </ul> 
-      </div>
-    );
+
+    componentDidMount() {
+        axios.get('http://localhost:5000/api/collections')
+            .then(res => {
+                var cards = res.data;
+                this.setState({
+                 cards: cards,
+                 loading: false});
+            })
+    }
+    render() {
+        if (this.state.loading === true) {
+            return (<div>loading...</div>);
+        }
+            let collection = this.state.cards[this.state.currentCollection];
+            let wordsmith = this.state.cards[this.state.currentCollection+1];
+            let dynamite = this.state.cards[this.state.currentCollection+2];
+            let vocabulary = [];
+            for(let i = 0;i < collection.cards.length; i++){
+            for(let i = 0;i < wordsmith.cards.length; i++) {
+            for(let i = 0;i < dynamite.cards.length; i++) {
+
+            
+            
+                vocabulary.push(
+                    <div>
+                        <p>
+                            {collection.cards[i].word}
+                            {collection.cards[i].definition}
+                            
+                        </p>
+                        <p>
+                            {wordsmith.cards[i].word}
+                            {wordsmith.cards[i].definition}
+                        </p>
+                        <p>
+                            {dynamite.cards[i].word}
+                            {dynamite.cards[i].definition}
+
+                        </p>
+
+
+                    </div>
+                );
+
+                }}}    
+            
+            return (
+
+            <div className="trialRun">
+                  {vocabulary}  
+                    
+
+
+
+            </div>);
     }
 }
-
 export default App;
